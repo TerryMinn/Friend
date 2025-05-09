6113a7b72bfde054a207017c4d415cc9c459e647
TerryMinn <shinnthantmindev.mm@gmail.com>
2025-05-09 14:53:39 +0700

    chore: next top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/components/layout/provider.client";
 import Nav from "@/components/layout/nav.server";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Provider>
           <Nav />
           {children}
+          <NextTopLoader color="gray" />
         </Provider>
       </body>
     </html>
